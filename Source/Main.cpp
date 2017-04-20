0dc95664352dba2b7bed013ea5802b4916c4ccb5
Colin Hill <colin.james.hill@gmail.com>
2017-04-20 07:51:48 -0700

    Begin sketching out layer building interfaces

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,9 +1,12 @@
 #pragma once
 
-#include "MultilayerPerceptron.h"
+#include "Neural/MultilayerPerceptron.h"
 
 int main()
 {
+    MultilayerPerceptron perceptron;
+    perceptron.BuildLayer(InputLayer::Builder().Size(28 * 28));
+    perceptron.BuildLayer(FullyConnectedLayer::Builder());
 
     return 0;
 }
